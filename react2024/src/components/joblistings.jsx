1d67347a7e7200dfd9f97d2bf842f561c7694c8a
dominique Taplin <Dtaplin21@aol.com>
2024-05-07 20:23:58 -0700

    add card info

--- react2024/src/components/joblistings.jsx
+++ react2024/src/components/joblistings.jsx
@@ -10,19 +10,19 @@ const joblistings = () => {
         Browse Jobs
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {jobs.map((job) => (
-         <div className="bg-white rounded-xl shadow-md relative">
+        {jobs.map((job, index) => (
+         <div key={index} className="bg-white rounded-xl shadow-md relative">
          <div className="p-4">
            <div className="mb-6">
              <div className="text-gray-600 my-2">{job.type}</div>
-             <h3 className="text-xl font-bold">Senior React Developer</h3>
+             <h3 className="text-xl font-bold">{ job.title }</h3>
            </div>
 
            <div className="mb-5">
-            We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+           { job.description }
            </div>
 
-           <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+           <h3 className="text-indigo-500 mb-2">{ job.salary }</h3>
 
            <div className="border border-gray-100 mb-5"></div>
 
