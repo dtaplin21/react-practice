98dc05ed994b6ea0392ed7e45eb21d3ab9981b83
dominique Taplin <Dtaplin21@aol.com>
2024-05-14 22:38:51 -0700

    jobs

--- react2024/src/App.jsx
+++ react2024/src/App.jsx
@@ -17,13 +17,7 @@ const App = () => {
 
   {/* <!-- Browse Jobs --> */}
  
-  <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
+  
     </>
   )
 }
