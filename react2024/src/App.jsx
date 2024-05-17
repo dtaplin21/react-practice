3d957d162636361c516ad56527d2b19b0a021eee
dominique Taplin <Dtaplin21@aol.com>
2024-05-16 22:59:17 -0700

    add

--- react2024/src/App.jsx
+++ react2024/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/navbar"
 import Hero from "./components/hero"
 import HomeCards from "./components/homecards"
 import JobListings from "./components/joblistings"
+import ViewAllJobs from ".components/viewalljobs"
 
 const App = () => {
   return (
@@ -10,13 +11,7 @@ const App = () => {
     <Hero title='Test Title' subtitle='this is the subtitle' />
     <HomeCards />
     <JobListings />
-  
-
-  {/* <!-- Developers and Employers --> */}
-  
-
-  {/* <!-- Browse Jobs --> */}
- 
+    <ViewAllJobs />
   
     </>
   )
