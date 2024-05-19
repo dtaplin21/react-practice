773dc2b3c26c8168b9ba6ad462af102d8fde97c1
dominique Taplin <Dtaplin21@aol.com>
2024-05-19 09:38:19 -0700

    add

--- react2024/src/App.jsx
+++ react2024/src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar"
 import Hero from "./components/hero"
 import HomeCards from "./components/homecards"
 import JobListings from "./components/joblistings"
-import ViewAllJobs from ".components/viewalljobs"
+import ViewAllJobs from "./components/viewalljobs"
 
 const App = () => {
   return (
